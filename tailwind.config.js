e48eea7e5ee3167eecfb8a0e18edba0d91d0d4ca
I Nyoman Budiarta <105336493+mangbudiarta@users.noreply.github.com>
2024-05-10 10:48:33 +0800

    Initial commit: Set up project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#22c55e",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
